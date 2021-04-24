205daf1df12e0c0ace5f9a9c3a3b3ba862eee630
GBehrends <82614773+GBehrends@users.noreply.github.com>
2021-04-24 16:07:25 -0500

    Create 05_filter_scripts.sh

---
+++ 05_filter_scripts.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --job-name=keep 
+#SBATCH --nodes=1 --ntasks=12
+#SBATCH --partition quanah
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --array=1-123
+
+# define input files from helper file during genotyping
+input_array=$( head -n${SLURM_ARRAY_TASK_ID} helper6.txt | tail -n1 )
+
+# define main working directory
+workdir=/lustre/scratch/gbehrend/EthiopianBirdsProject/Group 
+
+#Use different --thin filter settings to space SNPs apart to limit the effects of linkage on downstream analyses 
+
+# run vcftools with SNP output spaced 10kbp
+vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf --keep keeplist.txt --max-missing 1.0 --minQ 20 --minGQ 20 --minDP 8 --max-meanDP 50 --min-al
+leles 2 --max-alleles 2 --mac 1 --thin 10000 --max-maf 0.49 --remove-indels --recode --recode-INFO-all --out ${workdir}/10kbp_filtered_vcf/${input_
+array}
+
+# run vcftools with SNP output spaced 20kbp
+vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf --keep keeplist.txt --max-missing 1.0 --minQ 20 --minGQ 20 --minDP 8 --max-meanDP 50 --min-al
+leles 2 --max-alleles 2 --mac 1 --thin 20000 --max-maf 0.49 --remove-indels --recode --recode-INFO-all --out ${workdir}/20kbp_filtered_vcf/${input_
+array}
+

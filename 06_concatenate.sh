ca318eed672d94cc82ac5775d54ffa26ba9102ac
GBehrends <82614773+GBehrends@users.noreply.github.com>
2021-05-03 12:15:37 -0500

    Update 06_concatenate.sh

--- 06_concatenate.sh
+++ 06_concatenate.sh
@@ -10,17 +10,5 @@ cd <working directory>
 bcftools concat 20kbp_filtered_vcf/*.recode.vcf > 20kbpfiltered.recode.vcf
 bcftools concat 10kbp_filtered_vcf/*.recode.vcf > 10kbpfiltered.recode.vcf
 
-#Separate the new combined vcf file into vcf files corresponding to each sample 
-for file in 20kbpfiltered.recode.vcf; do
-  for sample in `bcftools query -l $file`; do
-    bcftools view -c1 -Oz -s $sample -o ${file/.vcf*/.$sample.vcf.gz} $file
-  done
-done
-
-for file in 10kbpfiltered.recode.vcf; do
-  for sample in `bcftools query -l $file`; do
-    bcftools view -c1 -Oz -s $sample -o ${file/.vcf*/.$sample.vcf.gz} $file
-  done
-done
 
 #Export these new vcf.gz files to local computer

a865fb04a13855bc95f990597738243b3a353d5d
GBehrends <82614773+GBehrends@users.noreply.github.com>
2022-02-26 13:55:16 -0600

    Update and rename Set_Length_and_Plot.R to 04_Modify_Lengths.r

--- RAxML/04_Modify_Lengths.r
+++ RAxML/04_Modify_Lengths.r
@@ -1,16 +1,19 @@
-## Setting Branch Lengths for ASTRAL III Trees
+## Setting Branch Lengths for ASTRAL III Trees and Plotting Them ##
 
 library(ape)
 library(phytools)
 library(stringr)
-setwd("/Volumes/T7/Scripts_and_Data/RAxML/ASTRAL III Output")
 
-x_files <- list.files("/Volumes/T7/Scripts_and_Data/RAxML/ASTRAL III Output", 
+branch_length <- "<your branch length>"
+working_directory <- "<directory with ASTRAL trees>"
+
+
+x_files <- list.files(working_directory, 
                       pattern = "_Astral.tre")
 for (i in 1:length(x_files)){
   x <- read.tree(x_files[i])
   x$edge.length[is.nan(x$edge.length)] <- NA
-  x$edge.length[is.na(x$edge.length)] <- 0.003
+  x$edge.length[is.na(x$edge.length)] <- branch_length
   write.tree(x, file = paste0(gsub(".tre", "", x_files[i]), "_corrected.tre"))
   pdf(file = paste0(gsub(".tre", "", x_files[i]), ".pdf"), height = 6,
       width = 6)

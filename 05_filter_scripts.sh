2d0db414a811f52de95d7b4166181dc86e8e98c9
GBehrends <82614773+GBehrends@users.noreply.github.com>
2022-02-27 15:42:55 -0600

    Update 05_filter_scripts.sh

--- 05_filter_scripts.sh
+++ 05_filter_scripts.sh
@@ -1,40 +1,42 @@
 #!/bin/sh
 #SBATCH --chdir=./
-#SBATCH --job-name=keep 
+#SBATCH --job-name=filt_species
 #SBATCH --nodes=1 --ntasks=12
 #SBATCH --partition quanah
 #SBATCH --time=48:00:00
 #SBATCH --mem-per-cpu=8G
-#SBATCH --array=1-123
+#SBATCH --array=1-<number of genotpyed scaffolds in the helper6.txt file 
+
+## A script to filter vcf files for STRUCTURE, DAPC, PCA, RAxML, MSMC2, Reich's FST and Calculating Obsevred Heterozygosity
+## Add this to each species directory in the 12_filter_scripts subdirectory
+                
+                                                        ###Beginning Steps###
+
+# define input files from helper file during genotyping
+. ~/conda/etc/profile.d/conda.sh
+conda activate vcftools
 
 # define input files from helper file during genotyping
 input_array=$( head -n${SLURM_ARRAY_TASK_ID} helper6.txt | tail -n1 )
 
-# define main working directory
-workdir=/lustre/scratch/gbehrend/EthiopianBirdsProject/<species directory> 
+##  define main working directory
+workdir=<species_directory>
 
-#Use different --thin filter settings to space SNPs apart to limit the effects of linkage on downstream analyses 
+# GATK 4.2.3 did not give genotype quality calls anymore, so remove the quality call filters for VCFtools. It should be fine if the average depth of coverage is ~20. 
+# Filter for Observed Heterozygosity 
+vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf  --max-missing 0.05 --minDP 6 --max-meanDP 50  --max-alleles 2  --max-maf 0.49 --remove-indels --recode --recode-INFO-all --out ${workdir}/NoThin_Vcf_0.5/${input_array}
 
-#keeplist.txt is a list of all sample IDs within the combined vcf files that you want to isolate. In my Ethiopian Birds Project, I did it for my 6 focal species individually. 
+# Filter for STRUCTURE/DAPC 10kbp
+# vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf  --max-missing 1.0  --minDP 6 --max-meanDP 50 --min-alleles 2 --mac 2 --max-alleles 2  --max-maf 0.49 --thin 20000 --remove-indels --recode --recode-INFO-all --out ${workdir}/10kbp_filtered_vcf/${input_array}
 
-# run vcftools with SNP output spaced 10kbp
-mkdir ${workdir}/10kbp_filtered_vcf
+# Filter for STRUCTURE/DAPC 20kbp
+# vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf  --max-missing 1.0  --minDP 6 --max-meanDP 50 --min-alleles 2 --max-alleles 2  --mac 2 --max-maf 0.49 --thin 10000 --remove-indels --recode --recode-INFO-all --out ${workdir}/20kbp_filtered_vcf/${input_array}
 
-vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf  --max-missing 1.0 --minQ 20 --minGQ 20 --minDP 8 --max-meanDP 50 --min-al
-leles 2 --max-alleles 2 --mac 1 --thin 10000 --max-maf 0.49 --remove-indels --recode --recode-INFO-all --out ${workdir}/10kbp_filtered_vcf/${input_
-array}
+# Filter for RAxML
+vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf  --max-missing 0.8  --minDP 6 --max-meanDP 50  --max-alleles 2  --max-maf 0.49 --remove-indels --recode --recode-INFO-all --out ${workdir}/phylo_vcf/${input_array}
 
-# run vcftools with SNP output spaced 20kbp
-mkdir ${workdir}/20kbp_filtered_vcf
+# Filter for MSMC
+vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf  --max-missing 0.05  --minDP 6 --max-meanDP 50  0 --max-alleles 2  --max-maf 0.49 --remove-indels --recode --recode-INFO-all --out ${workdir}/msmc_vcf/${input_array}
 
-vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf  --max-missing 1.0 --minQ 20 --minGQ 20 --minDP 8 --max-meanDP 50 --min-al
-leles 2 --max-alleles 2 --mac 1 --thin 20000 --max-maf 0.49 --remove-indels --recode --recode-INFO-all --out ${workdir}/20kbp_filtered_vcf/${input_
-array}
 
-# Run vcftools with no thinning filter 
-mkdir ${workdir}/NoThin_Vcf
-
-vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf  --max-missing 1.0 --minQ 20 --minGQ 20 --minDP 8 --max-meanDP 50 --min-al
-leles 2 --max-alleles 2 --mac 1 --max-maf 0.49 --remove-indels --recode --recode-INFO-all --out ${workdir}/NoThin_Vcf/${input_
-array}
 

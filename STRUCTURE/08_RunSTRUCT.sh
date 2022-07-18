2d8b099f1a71cb5531d04cf76e50c04f12d639a6
Garrett Behrends <82614773+GBehrends@users.noreply.github.com>
2022-07-18 14:51:23 -0500

    Update 08_RunSTRUCT.sh

--- STRUCTURE/08_RunSTRUCT.sh
+++ STRUCTURE/08_RunSTRUCT.sh
@@ -1,24 +1,21 @@
 #!/bin/bash 
-#SBATCH --job-name=strct_species
+#SBATCH --job-name=strct_Zosterops_poliogastrus
 #SBATCH --output=%x.%j.out
 #SBATCH --error=%x.%j.err
-#SBATCH --partition=nocona
+#SBATCH --partition=?
 #SBATCH --nodes=1
-#SBATCH --ntasks=8
+#SBATCH --ntasks=4
 #SBATCH --mem-per-cpu=8G
 
+# Load conda environment with STRUCTURE installed 
 . ~/conda/etc/profile.d/conda.sh
-conda activate strct
+conda activate <env_name>
 
-# Set paths for less typing
-input=<path to input structure file>
-output=<path to output>
-
-# Note that this script was run on STRUCTURE files formatted so that each individual's genotypes 
-# comprised two rows and not two columns. Gathering the info on number of loci and individuals 
-# from the structure files themselves assists in iterating by for loop if you chose to do so. 
-structure -K 3 -N $(cut -f1 ${input} | uniq | wc -l) \
--L $(awk '{print NF-3; exit}' ${input}) \
--i ${input} \
--o ${output}_replicate \
--D $RANDOM 
+# SampleID.txt was a list of all samples used here to calculate the number of individuals in the input structure file. -L was the number of loci with 3
+# subtracted to account for the added columns containing things like population info. -D was was set to a random number for the starting seed. 
+structure -K 3 \
+-N $(awk 'END{print NR}' ../../Zosterops_poliogastrus/00fastq/SampleID.txt) \
+-L $(awk '{print NF-3; exit}' ../strct_files/Zosterops_poliogastrus_50000_corrected.structure) \
+-i ../strct_files/Zosterops_poliogastrus_50000_corrected.structure \
+-o ../outdir/Zosterops_poliogastrus_50_replicate \
+-D $RANDOM

2d28b4d31d87f1b8c737de34b40febeaba935283
Garrett Behrends <82614773+GBehrends@users.noreply.github.com>
2022-12-08 15:28:00 -0600

    Update and rename Filt_Zip.sh to 03_FiltZip.sh

--- RAxML/03_FiltZip.sh
+++ RAxML/03_FiltZip.sh
@@ -5,7 +5,7 @@
 #SBATCH --partition=quanah
 #SBATCH --time=48:00:00
 #SBATCH --mem-per-cpu=8G
-#SBATCH --array=2-41
+#SBATCH --array=1-41
 
 # Original number of chromosomes -> 41
                 
@@ -18,13 +18,15 @@ conda activate vcftools
 input_array=$( head -n${SLURM_ARRAY_TASK_ID} vcf_list.txt | tail -n1 )
 
 ##  define main working directory
-workdir=/lustre/scratch/gbehrend/EthiopianBirdsProject/RAxML
+workdir=<output directory>
 
 # For loop through remeaining species
 for i in $(cat ../../specieslist); do 
 
-# Filter out individuals from target species and keep one outgroup as secificied by the keeplist. 
-vcftools --gzvcf ${workdir}/VCF/${input_array}.vcf.gz  --keep ${i}_keeplist.txt --recode --recode-INFO-all --out ${workdir}/${i}/${input_array}
+# Filter out individuals from target species and keep one outgroup as secificied by the keeplist. # Specifying max missing filter again. 20% missing info
+# out of all individuals in the group VCF will be different from the percentage of missing individuals per site in the species VCF so this filter is appplied 
+# here once more. 
+vcftools --gzvcf ${workdir}/VCF/${input_array}.vcf.gz  --max-missing 0.8 --keep ${i}_keeplist.txt --recode --recode-INFO-all --out ${workdir}/${i}/${input_array}
 
 # Bgzip and index the new vcf file 
 bgzip -c ${workdir}/${i}/${input_array}.recode.vcf > ${workdir}/${i}/${input_array}.vcf.gz

965b26ab7ff30be7a162b257f9d7b79c0ba49e24
Garrett Behrends <82614773+GBehrends@users.noreply.github.com>
2022-12-08 15:02:15 -0600

    Update and rename RAxML/01_Run_RAxML.r to Phylogenies/Make_RAxML_sh.r

--- Phylogenies/Make_RAxML_sh.r
+++ Phylogenies/Make_RAxML_sh.r
@@ -1,22 +1,32 @@
 
 #### R Script for Generating RAxML Pipeline by Joseph Manthey @ https://github.com/jdmanthey/certhia_phylogeography/blob/main/08a_phylo_stats_50kbp.r
-## Modify for your own directory as well as with the window size you want. This will generate a script to run RAxML. 
+## Modify for your own directory as well as with the window size you want. This will generate a shell script to run RAxML on windowed gene trees of a 
+## specified size. 
 
 options(scipen=999)
-	project_directory <- "<RAxML Directory>"
-	directory_name <- "<Window Size>KBP"
-	cluster <- "quanah"
-	max_number_jobs <- 400
+	project_directory <- <directory to place sh script outputs in> 
+	directory_name <- "species"
+	cluster <- <name of computing cluster>
+	max_number_jobs <- 1000
+	n_tasks <- "6"
+	output_sh <- "phylo50KBP_array.sh"
+	
+	# Location of VCF files to be used and the vcf to get the header from for all windows
+	VCF_location <- "/lustre/scratch/gbehrend/EthiopianBirdsProject/RAxML/species"
+	start_VCF <- "NC_054767.1.vcf.gz"
+	popmap_location <- paste(project_directory, "/species/Group_PopMap.txt", sep = "")
 	
 	# read in reference index
 	# filtered to only include genotyped chromosomes
-	ref_index <- read.table("<location of modified reference index", stringsAsFactors=F)
+	ref_index <- read.table(<path to file>, stringsAsFactors=F)
 	
-	# define window size
-	window_size <- 150000
-	
+	# define window size for gene trees 
+	window_size <- 50000
+
+
+###########################################################################################	
 	# make directories
-	dir.create(directory_name)
+	# dir.create(directory_name)
 	
 	# define intervals and write to helper files
 	tree_helper1 <- list()
@@ -66,17 +76,26 @@ options(scipen=999)
 	write(tree_helper3, file=paste(directory_name, "/tree_helper_end.txt", sep=""), ncolumns=1)
 
 	# write the array script
-	a.script <- paste(directory_name, "/phylo50kbp_array.sh", sep="")
+	a.script <- paste(directory_name, "/", output_sh, sep="")
 	write("#!/bin/sh", file=a.script)
 	write("#SBATCH --chdir=./", file=a.script, append=T)
 	write(paste("#SBATCH --job-name=", "phylo", sep=""), file=a.script, append=T)
-	write("#SBATCH --nodes=1 --ntasks=2", file=a.script, append=T)
+	write(paste("#SBATCH --nodes=1 --ntasks=", n_tasks, sep=""), file=a.script, append=T)
 	write(paste("#SBATCH --partition ", cluster, sep=""), file=a.script, append=T)
 	write("#SBATCH --time=48:00:00", file=a.script, append=T)
 	write("#SBATCH --mem-per-cpu=8G", file=a.script, append=T)
 	write(paste("#SBATCH --array=1-", n_array_jobs, sep=""), file=a.script, append=T)
 	write("", file=a.script, append=T)
-	write("module load intel R", file=a.script, append=T)
+	
+	write(paste("# Original num of jobs ->", n_array_jobs), file=a.script, append=T)
+	write("", file=a.script, append=T)
+	
+	write("# Activate Conda environment and modules", file=a.script, append=T)
+	write(". ~/conda/etc/profile.d/conda.sh", file=a.script, append=T)
+	write("conda activate vcftools", file=a.script, append=T)
+	write("", file=a.script, append=T)
+	write("module load gcc/10.1.0", file=a.script, append=T)
+	write("module load r/4.0.2", file=a.script, append=T)
 	write("", file=a.script, append=T)
 
 	write("# Set the number of runs that each SLURM task should do", file=a.script, append=T)
@@ -105,45 +124,50 @@ options(scipen=999)
 	write("\tend_array=$( head -n${run} tree_helper_end.txt | tail -n1 )", file=a.script, append=T)
 	write("", file=a.script, append=T)
 	
+	
 	# add header to output file
-	header <- paste('\tgunzip -cd ', project_directory, '/Ca_0029_Tg_19.recode.vcf.gz | grep "#" > ', project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.recode.vcf", sep="")
+	header <- paste('\tgunzip -cd ', VCF_location, "/", start_VCF, ' | grep "#" > ', project_directory, "/", directory_name, "/windows/${chrom_array}__${start_array}__${end_array}.vcf", sep="")
 	write(header, file=a.script, append=T)
 	write("", file=a.script, append=T)
 	
 	#tabix command
-	tabix_command <- paste("\ttabix ", project_directory, "/${chrom_array}.recode.vcf.gz ${chrom_array}:${start_array}-${end_array} >> ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.recode.vcf", sep="")
+	tabix_command <- paste("\ttabix ", VCF_location, "/${chrom_array}.vcf.gz ${chrom_array}:${start_array}-${end_array} >> ", project_directory, "/", directory_name, "/windows/${chrom_array}__${start_array}__${end_array}.vcf", sep="")
 	write(tabix_command, file=a.script, append=T)
 	write("", file=a.script, append=T)
 	
 	# bcftools command
-	bcf_tools_command <- paste("\tbcftools query -f '%POS\\t%REF\\t%ALT[\\t%GT]\\n' ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.recode.vcf > ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.simple.vcf", sep="")
+	bcf_tools_command <- paste("\tbcftools query -f '%POS\\t%REF\\t%ALT[\\t%GT]\\n' ", project_directory, "/", directory_name, "/windows/${chrom_array}__${start_array}__${end_array}.vcf > ", project_directory, "/", directory_name, "/windows/${chrom_array}__${start_array}__${end_array}.simple.vcf", sep="")
 	write(bcf_tools_command, file=a.script, append=T)
 	write("", file=a.script, append=T)
 	
 	# Rscript command for stats
-	rscript_command <- paste("\tRscript calculate_windows.r ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.simple.vcf popmap_phylo.txt", sep="")
+	rscript_command <- paste("\tRscript calculate_windows.r ", project_directory, "/", directory_name, "/windows/${chrom_array}__${start_array}__${end_array}.simple.vcf", " ", popmap_location, sep="")
 	write(rscript_command, file=a.script, append=T)
 	write("", file=a.script, append=T)
 	
 	# Rscript command for fasta creation
-	rscript_command <- paste("\tRscript create_fasta.r ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.simple.vcf popmap_phylo.txt", sep="")
+	rscript_command <- paste("\tRscript create_fasta.r ", project_directory, "/", directory_name, "/windows/${chrom_array}__${start_array}__${end_array}.simple.vcf", " ", popmap_location, sep="")
 	write(rscript_command, file=a.script, append=T)
 	write("", file=a.script, append=T)
 	
+	# sed command (writing this command in R does not work at the moment. The asterisk is tricky. Will have to manually edit sh scripts later with:
+	# sed -i 's/\*/?/g' <output fasta file path> 
+	
 	# raxml commands
-	raxml_command <- paste("\traxmlHPC-PTHREADS-SSE3 -T 2 -f a -x 50 -m GTRCAT -p 253 -N 100 -s ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.fasta -n ${chrom_array}__${start_array}__${end_array}.tre -w ", project_directory, "/windows/", sep="")
+	raxml_command <- paste("\t/lustre/work/johruska/miniconda2/bin/raxmlHPC-PTHREADS-SSE3 -T ", n_tasks, " -f a -x 150 -m GTRCAT -p 253 -N 100 -s", project_directory, "/", directory_name, "/windows/${chrom_array}__${start_array}__${end_array}.fasta -n ${chrom_array}__${start_array}__${end_array}.tre -w ", project_directory, "/", directory_name, "/windows/", sep="")
 	write(raxml_command, file=a.script, append=T)
 	write("", file=a.script, append=T)
 	
 	# remove unnecessary files at end
-	write(paste("\trm ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.recode.vcf", sep=""), file=a.script, append=T)
-	write(paste("\trm ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.simple.vcf", sep=""), file=a.script, append=T)
-	write(paste("\trm ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.fasta", sep=""), file=a.script, append=T)
-	write(paste("\trm ", project_directory, "/windows/RAxML_bestTree.${chrom_array}__${start_array}__${end_array}.tre", sep=""), file=a.script, append=T)
-	write(paste("\trm ", project_directory, "/windows/RAxML_bipartitionsBranchLabels.${chrom_array}__${start_array}__${end_array}.tre", sep=""), file=a.script, append=T)
-	write(paste("\trm ", project_directory, "/windows/RAxML_info.${chrom_array}__${start_array}__${end_array}.tre", sep=""), file=a.script, append=T)
+	write(paste("\trm ", project_directory, "/", directory_name, "/windows/${chrom_array}__${start_array}__${end_array}.vcf", sep=""), file=a.script, append=T)
+	write(paste("\trm ", project_directory, "/", directory_name, "/windows/${chrom_array}__${start_array}__${end_array}.simple.vcf", sep=""), file=a.script, append=T)
+	write(paste("\trm ", project_directory, "/", directory_name, "/windows/${chrom_array}__${start_array}__${end_array}.fasta", sep=""), file=a.script, append=T)
+	write(paste("\trm ", project_directory, "/", directory_name, "/windows/RAxML_bestTree.${chrom_array}__${start_array}__${end_array}.tre", sep=""), file=a.script, append=T)
+	write(paste("\trm ", project_directory, "/", directory_name, "/windows/RAxML_bipartitionsBranchLabels.${chrom_array}__${start_array}__${end_array}.tre", sep=""), file=a.script, append=T)
+	write(paste("\trm ", project_directory, "/", directory_name, "/windows/RAxML_info.${chrom_array}__${start_array}__${end_array}.tre", sep=""), file=a.script, append=T)
 	write("", file=a.script, append=T)
 	
 	# finish
 	write("done", file=a.script, append=T)
+
 	

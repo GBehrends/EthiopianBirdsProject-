a7264e86458afbbe5bfc8b6ad24a991f601dbfa2
Garrett Behrends <82614773+GBehrends@users.noreply.github.com>
2023-03-08 13:04:32 -0600

    Update 01_MSMC_Species.sh

--- MSMC2/01_MSMC_Species.sh
+++ MSMC2/01_MSMC_Species.sh
@@ -22,10 +22,17 @@ sed "s/species/${array}/g" ${workdir}/MSMC2/vcf_to_msmc.r > ${workdir}/MSMC2/${a
 # Convert vcfs to msmc format files for all species and their samples 
 Rscript ${workdir}/MSMC2/${array}_msmc.r
 
-# Add species msmc file paths to a list that will be used when running MSMC2. 
+# Add species msmc file paths to a list that will be used when running MSMC2 (helper1.txt):
 cd ${array}_demography
 for i in $(find ~+ -type d | sed '1d' | sed 's,\./,,g'); do echo "${i}/*txt" >> \
 ${workdir}/MSMC2/helper1.txt; done
+
+# MSMC2 output prefixes (helper2.txt): 
+for i in $(find ~+ -type d | sed '1d' | sed 's,\./,,g'); do echo "${i}" >> \
+${workdir}/MSMC2/helper2.txt; done
+for i in $(find ~+ -type d | sed '1d' | sed 's,\./,,g'); do for x in {1..10}; do \
+echo "${i}_b${x}" >> ${workdir}/MSMC2/helper2.txt; done
+
  
 
 
